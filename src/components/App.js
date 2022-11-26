a811434e6f48db262acd079e37e06411bf845ef8
+ <+>
2022-11-25 20:45:29 -0500

    pushing App update

---
+++ src/components/App.js
@@ -0,0 +1,37 @@
+import React, {useState, useEffect} from "react"
+const reviewsData = "http://localhost:3000/reviews"
+
+function App() {
+  const [allReviews, setAllReviews] = useState([])
+
+useEffect(() => {
+    fetch(reviewsData)
+        .then((data) => data.json())
+        .then((reviews) => {
+            setAllReviews(reviews)
+        })
+}, [])
+console.log("allReviews", allReviews)
+  return (
+    <div>
+    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    //</div>
+  );
+}
+
+export default App;
