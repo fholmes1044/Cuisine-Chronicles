d4ca2359d6b5437ec3127b94f593eefc649165fc
+ <+>
2023-09-26 22:43:56 -0400

    added cors to the server.js

--- node-backend/server.js
+++ node-backend/server.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 const reviewRoutes = require('./routes/reviewRoutes');
 
 app.use(bodyParser.json());
+
+const corsOptions = {
+    origin: 'http://localhost:3000', 
+  };
+
+app.use(cors(corsOptions));
+
+
 
 app.use('/api', reviewRoutes);
 
