a02bb59b95473c435c14d00a3e8337e41f23aea2
+ <+>
2023-09-27 18:01:40 -0400

    added user feature of searching for recipe recommendations through the edamam api

--- src/components/NewRecipeSearch.js
+++ src/components/NewRecipeSearch.js
@@ -4,16 +4,19 @@ function NewRecipeSearch() {
   const [recipes, setRecipes] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
   const apiKey = process.env.REACT_APP_EDAMAM_API_KEY;
+  const appId = "dd219920";
 
   const searchRecipes = async () => {
     try {
       const response = await fetch(
-        `https://api.edamam.com/search?q=${searchQuery}&app_key=${apiKey}`
+        `https://api.edamam.com/search?q=${searchQuery}&app_id=${appId}&app_key=${apiKey}`
       );
 
       if (response.ok) {
         const data = await response.json();
+        console.log("data", data)
         setRecipes(data.hits);
+        console.log(recipes)
       } else {
         console.error("Error fetching recipes");
       }
@@ -21,6 +24,11 @@ function NewRecipeSearch() {
       console.error("Error fetching recipes", error);
     }
   };
+
+  useEffect(() => {
+    console.log("Updated recipes:", recipes); 
+  }, [recipes]); 
+  
 
   useEffect(() => {
     if (searchQuery) {
